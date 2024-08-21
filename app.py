f731798efc5ad1d9845c067167be282eb8a549ed
matansol <matan.solomo@gmail.com>
2024-08-21 11:27:36 +0300

    socketio.run

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def load_agent(env, model_path):
     return ppo
 
 if __name__ == '__main__':
-    create_database()
+    # create_database()
     players_sessions = {}
     env = CustomEnv(render_mode="rgb_array")
     env = ObjObsWrapper(env)
@@ -266,4 +266,5 @@ if __name__ == '__main__':
                     9: "minigrid_custom_20240804/iter_10^6_steps"
     }
     manual_control = ManualControl(env, model_paths)
+    socketio.run(app, debug=True)
     # socketio.run(app, debug=True, host='0.0.0.0', port=8000)
